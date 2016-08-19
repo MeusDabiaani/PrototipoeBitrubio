55e3093e163d028e2d11d278bf5c00c1a430edd5
MeusDabiaani <autorizaciones@meustech.com>
2016-08-19 10:06:37 -0500

    iconos metas chicos

--- app/src/main/java/com/bitrubio/prototipoebitrubio/Entidades/Bitrubian.java
+++ app/src/main/java/com/bitrubio/prototipoebitrubio/Entidades/Bitrubian.java
@@ -11,8 +11,11 @@ public class Bitrubian {
     String mail;
     String success;
     String idUsuario;
+
     public Bitrubian() {}
-    public Bitrubian(int id, String nombre, String ape_pat, String mail, String success,String idUsuario) {
+
+
+    public Bitrubian(int id, String nombre, String ape_pat, String mail, String success, String idUsuario) {
         this.id = id;
         this.nombre = nombre;
         this.ape_pat = ape_pat;
@@ -25,25 +28,48 @@ public class Bitrubian {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getApe_pat() {
         return ape_pat;
     }
 
+    public void setApe_pat(String ape_pat) {
+        this.ape_pat = ape_pat;
+    }
+
     public String getMail() {
         return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 
     public String getSuccess() {
         return success;
     }
 
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
     public String getIdUsuario() {
         return idUsuario;
     }
 
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 }
 
